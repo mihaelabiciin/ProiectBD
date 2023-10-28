cebd0bab3a44392b5c031800d36528845aef4899
HPMIHAELA\mihae <mihaela.biciin24@gmail.com>
2023-10-28 16:28:07 +0300

    Added dropdown with Locatii in Hoteluri page

--- Frontend/src/app/components/hoteluri/hoteluri.component.ts
+++ Frontend/src/app/components/hoteluri/hoteluri.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Locatie } from 'src/app/models/locatie';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-hoteluri',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./hoteluri.component.css']
 })
 export class HoteluriComponent {
+  locatii: Locatie[] = []; 
+  selectedLocatie: Locatie | null = null;
 
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+    
+  }
+  ngOnInit() {
+    this.getLocatii();
+    
+  }
+  getLocatii() {
+    return this.http.get(`${environment.baseUrl}/Locatii` ).subscribe(
+      (result) => {
+        console.log(result)
+        this.locatii = result as Locatie[];
+        console.log(this.locatii)
+      },
+      (error) => {
+        // Handle errors here, such as displaying an error message
+        this.snackBar.open("Nu se pot incarca locatiile");
+      }
+    );
+  }
+
+  addHotel() {
+    //hotel: Hotel = new Hotel(nume, idLocatie..);
+  }
 }
